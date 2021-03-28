7fe9f81246e4d4b0a806a8ceee8896e1da858f74
Wojciech Bielecki <wojbiel833@gmail.com>
2021-03-28 16:25:46 +0200

    Add header

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './Hero.scss';
+
+class MyComponent extends React.Component {
+  render() {
+    return (
+      <header className={styles.component}>
+        <h2 className={styles.title}>Things to do</h2>
+        <img
+          className={styles.image}
+          src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png"
+        />
+      </header>
+    );
+  }
+}
+
+export default MyComponent;
