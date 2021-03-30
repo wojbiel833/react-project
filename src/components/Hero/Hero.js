a52a1f7657a30da227687e0351c5cd0aef306206
Wojciech Bielecki <wojbiel833@gmail.com>
2021-03-30 16:52:45 +0200

    Add Creator compnent

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import ReactHtmlParser from 'react-html-parser';
 import PropTypes from 'prop-types';
 import styles from './Hero.scss';
 
 const Hero = props => (
   <header className={styles.component}>
-    <h2 className={styles.title}>{props.titleText}</h2>
+    <h2 className={styles.title}>{ReactHtmlParser(props.titleText)}</h2>
     <img className={styles.image} src={props.imgSrc} />
   </header>
 );
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,32 +1,67 @@
 import React from 'react';
+import ReactHtmlParser from 'react-html-parser';
 import PropTypes from 'prop-types';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
 import Column from '../Column/Column.js';
+import Creator from '../Creator/Creator.js';
+import { settings } from '../../data/dataStore';
 
 class List extends React.Component {
+  state = {
+    columns: this.props.columns || [],
+  };
   static propTypes = {
     title: PropTypes.node.isRequired,
-    children: PropTypes.node,
+    description: PropTypes.node,
     src: PropTypes.string,
+    columns: PropTypes.array,
   };
   static defaultProps = {
-    //setting default on {this.props.children}
-    children: <p>I can do all the things!</p>,
+    description: settings.defaultListDescription,
   };
+
+  addColumn(title) {
+    this.setState(state => ({
+      columns: [
+        ...state.columns,
+        {
+          key: state.columns.length
+            ? state.columns[state.columns.length - 1].key + 1
+            : 0,
+          title,
+          icon: 'list-alt',
+          cards: [],
+        },
+      ],
+    }));
+  }
+
   render() {
     console.log('Props', this.props);
+    console.log('State', this.state);
     return (
       <section className={styles.component}>
         <Hero titleText={this.props.title} imgSrc={this.props.src} />
-        <div className={styles.description}>{this.props.children}</div>
+        <div className={styles.description}>
+          {ReactHtmlParser(this.props.description)}
+        </div>
         <div className={styles.columns}>
-          <Column title="Column 1" />
-          <Column title="Column 2" />
-          <Column title="Column 3" />
+          {this.state.columns.map(({ key, ...columnProps }) => (
+            <Column key={key} {...columnProps} />
+          ))}
+        </div>
+        <div className={styles.creator}>
+          <Creator
+            text={settings.columnCreatorText}
+            action={title => {
+              this.addColumn(title);
+            }}
+          />
         </div>
       </section>
     );
+    console.log('State', this.state);
   }
 }
 
