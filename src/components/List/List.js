76cdf0c60763849254cf8097fe0bed53002d23b7
Wojciech Bielecki <wojbiel833@gmail.com>
2021-03-31 10:38:46 +0200

    Fix splash display

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -43,7 +43,7 @@ class List extends React.Component {
     // console.log('State', this.state);
     return (
       <section className={styles.component}>
-        <Hero titleText={this.props.title} imgSrc={this.props.src} />
+        <Hero titleText={this.props.title} imgSrc={this.props.image} />
         <div className={styles.description}>
           {ReactHtmlParser(this.props.description)}
         </div>
