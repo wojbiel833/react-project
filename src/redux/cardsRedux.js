003c8d95a1bca634bca15bcaf152ecd14262f32c
Wojciech Bielecki <wojbiel833@gmail.com>
2021-04-12 16:43:07 +0200

    ...

---
+++ src/redux/cardsRedux.js
@@ -0,0 +1,30 @@
+import shortid from 'shortid';
+// selectors
+export const getCardsForColumns = ({ cards }, columnId) =>
+  cards.filter(card => card.columnId == columnId);
+
+export const getCardsForSearch = ({ cards }, searchString) =>
+  cards.filter(card => new RegExp(searchString, 'i').test(card.title));
+
+// action name creator
+const reducerName = 'cards';
+const createActionName = name => `app/${reducerName}/${name}`;
+
+// action types
+export const ADD_CARD = createActionName('ADD_CARD');
+
+// action creators
+export const createActionAddCard = payload => ({
+  payload: { ...payload, id: shortid.generate() },
+  type: ADD_CARD,
+});
+
+// reducer
+export default function reducer(statePart = [], action = {}) {
+  switch (action.type) {
+    case ADD_CARD:
+      return [...statePart, action.payload];
+    default:
+      return statePart;
+  }
+}
